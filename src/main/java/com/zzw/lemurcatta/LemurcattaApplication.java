fa4ba515c56ee7adf63fa2ba77631870f616ff94
zzw <zhuangwei.zheng@hand-china.com>
2020-02-20 10:17:45 +0800

    构建项目

---
+++ src/main/java/com/zzw/lemurcatta/LemurcattaApplication.java
@@ -0,0 +1,13 @@
+package com.zzw.lemurcatta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemurcattaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemurcattaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzw/lemurcatta/LemurcattaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzw.lemurcatta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemurcattaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
