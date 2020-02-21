09b89e7601ccdaa2c60042a5298da27cb3879589
zzw <zhuangwei.zheng@hand-china.com>
2020-02-21 12:22:30 +0800

    upd

---
+++ springboot-study/study-redis-autoconfig/src/test/java/com/zzw/studyredisautoconfig/StudyRedisAutoconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzw.studyredisautoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRedisAutoconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
